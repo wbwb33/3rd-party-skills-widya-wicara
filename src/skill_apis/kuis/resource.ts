9a46a4d414c5828e70f73c6601ae99c46c069427
wbwb33 <wibi.s.wi@gmail.com>
2019-11-29 09:17:23 +0700

    fix quiz's main function push-to-docker

--- src/skill_apis/kuis/resource.ts
+++ src/skill_apis/kuis/resource.ts
@@ -9,37 +9,37 @@ class KuisData {
     const cat = Math.floor(Math.random()*14)+1;
     console.log(cat);
     await this.updateNewDay();
-    // fs.readFile(`dependent/kuis/${cat}.json`, (err, data) => {
-    //   if (err) {
-    //     console.log('data dependent kuis not found');
-    //   } else {
-    //     const content = JSON.parse(data.toString());
-    //     const id: number = Math.floor(Math.random()*(content.length-5));
-    //     console.log(id+5);
-    //     var kuisArr = [];
-    //     for(let i=id;i<(id+5);i++){
-    //       console.log(i);
-    //       kuisArr.push(content[i]);
-    //     }
-    //     content.splice(id,5);
-    //     console.log(kuisArr);
+    fs.readFile(`dependent/kuis/${cat}.json`, (err, data) => {
+      if (err) {
+        console.log('data dependent kuis not found');
+      } else {
+        const content = JSON.parse(data.toString());
+        const id: number = Math.floor(Math.random()*(content.length-5));
+        console.log(id+5);
+        var kuisArr = [];
+        for(let i=id;i<(id+5);i++){
+          console.log(i);
+          kuisArr.push(content[i]);
+        }
+        content.splice(id,5);
+        console.log(kuisArr);
 
-    //     fs.writeFile('cache/kuis_today.json', JSON.stringify(kuisArr), (err) => {
-    //       if(err){
-    //         console.log('cannot write data kuis today, maybe folder not yet created');
-    //       } else {
-    //         console.log("done write quizes for today in cache");
-    //       }
-    //     });
-    //     fs.writeFile(`dependent/kuis/${cat}.json`, JSON.stringify(content), (err) => {
-    //       if(err){
-    //         console.log('cannot write new dependent data kuis');
-    //       } else {
-    //         console.log("done write new dependent data with deleted used quizes");
-    //       }
-    //     });
-    //   }
-    // });
+        fs.writeFile('cache/kuis_today.json', JSON.stringify(kuisArr), (err) => {
+          if(err){
+            console.log('cannot write data kuis today, maybe folder not yet created');
+          } else {
+            console.log("done write quizes for today in cache");
+          }
+        });
+        fs.writeFile(`dependent/kuis/${cat}.json`, JSON.stringify(content), (err) => {
+          if(err){
+            console.log('cannot write new dependent data kuis');
+          } else {
+            console.log("done write new dependent data with deleted used quizes");
+          }
+        });
+      }
+    });
   }
   
   /** this function will update all done_today to false, will ONLY GET CALLED AT NEW DAY */
