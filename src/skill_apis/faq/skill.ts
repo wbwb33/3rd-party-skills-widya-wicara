5f04bc4e544d0eec6f3620aaa53709b1fcf81de9
wbwb33 <wibi.s.wi@gmail.com>
2019-12-06 16:29:46 +0700

    fix faq's skill push-to-docker

--- src/skill_apis/faq/skill.ts
+++ src/skill_apis/faq/skill.ts
@@ -6,7 +6,8 @@ class FaqSkill {
   public index = async(req: Request, res: Response) => {
     const msg = req.query.msg;
     const respond = await this.findSimilar(msg);
-    console.log(respond);
+    // console.log(JSON.parse(respond));
+    res.send(JSON.parse(respond));
   }
 
   private findSimilar = async (str: string): Promise<string> => {
@@ -21,21 +22,20 @@ class FaqSkill {
         } else {
           const a = JSON.parse(data.toString());
           for(let i=0;i<a.length;i++){
-            if((i/1000)%1==0){
-              console.log(i);
-            }
+            // if((i/1000)%1==0){
+            //   console.log(i);
+            // }
             if(a[i].respond){
               var resp = a[i].respond;
             }
             var strCompare = a[i].keywords.toLowerCase().replace(/[^a-z|\s]+/g,"").split(" ");
             if(similarity(msg,strCompare,winkOpts).exact==strCompare.length){
-              let result = `{detected: ${strCompare},respond: ${resp}`;
-              console.log("detected:"+strCompare);
+              let result = `{"detected": "${a[i].keywords}","respond": ${JSON.stringify(resp)}}`;
+              // console.log("detected:"+strCompare);
               resolve(result);
               break;
             }
           }
-          // resolve(msg.toString());
         }
       })
     })
