cdcafb28ee94d688bc2aeea692a8b44cc7f01dee
wbwb33 <wibi.s.wi@gmail.com>
2020-01-03 11:01:47 +0700

    add response in currency, push-to-docker

--- src/skill_apis/currency/skill.ts
+++ src/skill_apis/currency/skill.ts
@@ -5,20 +5,23 @@ class CurrencySkill {
   public get = async(req: Request, res: Response) => {
     var hrstart = process.hrtime();
     const arr:any[] = await this.preprocessingMsg(req.query.msg);
-    console.log(arr);
+    // console.log(arr);
 
-    const arrMataUang = ["Euro", "Dolar Singapura", "Dolar Australia", "Dolar Hong Kong", "Dolar Kanada", "Dolar", "Ringgit", "Poundsterling", "Yuan", "Renmibi", "Lira", "Peso Meksiko", "Yen", "Won", "Riyal", "Dirham", "rupee", "Rufiyaa", "Baht", "Peso", "Forint", "Franc"];
-    // const arrNegara = ["Eropa", "Singapura", "Australia", "Hong Kong", "Kanada", "Amerika Serikat", "Malaysia", "Inggris", "China", "China", "Italia", "Meksiko", "Jepang", "Korea", "Saudi Arabia", "Arab", "India", "Maladewa (Maldives)", "Thailand", "Filipina", "Hungary", "Swiss"]; 
-    const arrKode = ["EUR","SGD","AUD","HKD","CAD","USD","MYR","GBP","CNY","CNH","ITL","MXN","JPY","KRW","SAR","AED","INR","MVR","THB","PHP","HUF","CHF"];
+    const arrMataUang = ["Euro", "Dolar Singapura", "Dolar Australia", "Dolar Hong Kong", "Dolar Kanada", "Dolar", "Rupiah", "Ringgit", "Poundsterling", "Yuan", "Renmibi", "Lira", "Peso Meksiko", "Yen", "Won", "Riyal", "Dirham", "rupee", "Rufiyaa", "Baht", "Peso", "Forint", "Franc"];
+    // const arrNegara = ["Eropa", "Singapura", "Australia", "Hong Kong", "Kanada", "Amerika Serikat", "Indonesia", "Malaysia", "Inggris", "China", "China", "Italia", "Meksiko", "Jepang", "Korea", "Saudi Arabia", "Arab", "India", "Maladewa (Maldives)", "Thailand", "Filipina", "Hungary", "Swiss"]; 
+    const arrKode = ["EUR","SGD","AUD","HKD","CAD","USD","IDR","MYR","GBP","CNY","CNH","ITL","MXN","JPY","KRW","SAR","AED","INR","MVR","THB","PHP","HUF","CHF"];
 
     let ar: any[] = [];
+    let arStrMataUang: string[] = [];
 
     for(let i=0;i<arrMataUang.length;i++){
       if(arr[0].includes(arrMataUang[i].toLowerCase())){
         ar[0] = arrKode[i];
+        arStrMataUang[0] = arrMataUang[i];
       }
       if(arr[1].includes(arrMataUang[i].toLowerCase())){
         ar[1] = arrKode[i];
+        arStrMataUang[1] = arrMataUang[i];
       }
       if(ar[0]&&ar[1]){
         break;
@@ -28,26 +31,28 @@ class CurrencySkill {
     ar[2]=arr[2];
 
     if(ar.length==3){
-      console.log(`${ar}`);
+      // console.log(`${ar}`);
     } else {
       ar[1] = "IDR";
-      console.log(`${ar}`);
+      // console.log(`${ar}`);
     }
 
     const final = await this.getDataFromLink(ar[0],ar[1],ar[2]);
-    console.log(final);
+    // console.log(final);
 
     const hrend = process.hrtime(hrstart);
-    console.log(`succesfull,in ${hrend[0]}s ${hrend[1]/1000000}ms `);
+    // console.log(`succesfull,in ${hrend[0]}s ${hrend[1]/1000000}ms `);
     res.send(JSON.parse(`{
       "status": "success",
       "action": "currency-found",
       "data": {
         "from": {
           "currency":"${ar[0]}",
+          "string":"${arStrMataUang[0]}",
           "value":"${ar[2]}"
         },"to": {
           "currency":"${ar[1]}",
+          "string":"${arStrMataUang[1]}",
           "value":"${final}"
         }
       },
