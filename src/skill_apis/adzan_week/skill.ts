8ab9f0e8012763ecc13bfe22b7e3baba2b6df2f6
wbwb33 <wibi.s.wi@gmail.com>
2020-08-26 10:06:22 +0700

    fix set today to apps for jadwal salat
    [build][patch]

--- src/skill_apis/adzan_week/skill.ts
+++ src/skill_apis/adzan_week/skill.ts
@@ -17,7 +17,7 @@ class AdzanWeekSkill {
     const offset = isNaN(+req.query.offset)?7:+req.query.offset;
     const specific = req.query.specific; // bool
 
-    if(specific) {
+    if(specific==true) {
       const salat = req.query.salat; // str
       const jam = req.query.jam; // HH:mm
 
